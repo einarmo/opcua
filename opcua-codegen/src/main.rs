3a4a15bbcfece56aafc691b0e9c7a60292372d19
Einar Omang <einar.omang@cognite.com>
2024-07-24 10:17:14 +0200

    Manage headers

--- opcua-codegen/src/main.rs
+++ opcua-codegen/src/main.rs
@@ -2,11 +2,14 @@ use opcua_codegen::{run_codegen, CodeGenConfig, CodeGenTarget, TypeCodeGenTarget
 
 fn main() {
     let config = CodeGenConfig {
-        extra_header: "".to_owned(),
+        extra_header: r#"// OPCUA for Rust
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (C) 2017-2024 Adam Lock, Einar Omang"#
+            .to_owned(),
         opcua_crate_path: "opcua".to_owned(),
         targets: vec![CodeGenTarget::Types(TypeCodeGenTarget {
-            file_path: "../tools/schema/schemas/1.0.4/Opc.Ua.Types.bsd".to_owned(),
-            output_dir: "../samples/gen-test/src/generated".to_owned(),
+            file_path: "tools/schema/schemas/1.0.4/Opc.Ua.Types.bsd".to_owned(),
+            output_dir: "samples/gen-test/src/generated".to_owned(),
             enums_single_file: true,
             ..Default::default()
         })],
